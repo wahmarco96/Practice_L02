10884d2ec69d9408088a2823da2954a645da76a5
MQ <shikiitori@gmail.com>
2018-10-12 23:22:51 +0800

    Stage1_Scene(Player Movement Only)

---
+++ Assets/Script/PlayerMovement.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class PlayerMovement : MonoBehaviour, IDragHandler, IEndDragHandler
+{
+
+    Vector3 initPos;
+    public Vector3 dir;
+    public Transform player;
+
+    // Use this for initialization
+    void Start()
+    {
+        initPos = this.transform.position;
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        this.transform.position = eventData.position;
+        dir = this.transform.position - initPos;
+        dir.Normalize();
+    }
+
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        this.transform.position = initPos;
+        dir = Vector3.zero;
+    }
+
+    void Update()
+    {
+        Move();
+    }
+
+    void Move()
+    {
+        Vector3 playerMove = Vector3.zero;
+        playerMove.x = dir.x;
+        playerMove.z = dir.y;
+        player.Translate(playerMove.x * Time.deltaTime, 0, playerMove.z * Time.deltaTime, Space.World);
+    }
+}
