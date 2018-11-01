bda5e2d71b6ef700f0456728ad45698f318303d0
MQ <shikiitori@gmail.com>
2018-11-02 00:02:01 +0800

    Transform[] spawningPosition, instantiated enemy cant detect player (problem not solved yet)

--- Assets/Script/Enemy.cs
+++ Assets/Script/Enemy.cs
@@ -4,29 +4,31 @@ using UnityEngine;
 using UnityEngine.AI;
 using UnityEngine.UI;
 
-public class Enemy : MonoBehaviour {
+public class Enemy : MonoBehaviour
+{
 
     NavMeshAgent agent;
     public Transform player;
     float maxHealth = 5f;
     public float currentHealth;
-    public bool playerAlive =true;
+    public bool playerAlive = true;
     public Image currentHealthBar;
     public bool playerInRange;
     public float takeDamage;
     public AttackController attackDamage;
-
     Vector3 originPos;
 
     // Use this for initialization
-    void Start () {
+    void Start()
+    {
         agent = GetComponent<NavMeshAgent>();
         currentHealth = maxHealth;
         originPos = this.transform.position;
     }
-	
-	// Update is called once per frame
-	void Update () {
+
+    // Update is called once per frame
+    void Update()
+    {
         StartCoroutine(CheckPlayerPos());
         IsDead();
         TakeDamage();
@@ -41,37 +43,11 @@ public class Enemy : MonoBehaviour {
             agent.SetDestination(player.position);
             yield return new WaitForSeconds(1);
         }
-        
-       else
+
+        else
         {
             agent.SetDestination(originPos);
         }
-    }
-
-    void OnCollisionEnter(Collision collision)
-    {
-        if (collision.gameObject.tag == "Player")
-        {
-            currentHealth -= takeDamage;
-        }
-    }
-
-    void IsDead()
-    {
-        if (currentHealth < 1f)
-        {
-            Destroy(this.gameObject);
-        }
-    }
-
-    void TakeDamage()
-    {
-        takeDamage = attackDamage.playerAttackDamage;
-    }
-
-    void CheckCurrentHealth()
-    {
-        currentHealthBar.fillAmount = currentHealth / maxHealth;
     }
 
     void NoticePlayerInRange()
@@ -83,6 +59,32 @@ public class Enemy : MonoBehaviour {
         else
         {
             playerInRange = false;
+        }
+    }
+
+    void TakeDamage()
+    {
+        takeDamage = attackDamage.playerAttackDamage;
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            currentHealth -= takeDamage;
+        }
+    }
+
+    void CheckCurrentHealth()
+    {
+        currentHealthBar.fillAmount = currentHealth / maxHealth;
+    }
+
+    void IsDead()
+    {
+        if (currentHealth < 1f)
+        {
+            Destroy(this.gameObject);
         }
     }
 }
---
+++ Assets/Script/SpawnPoint.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnPoint : MonoBehaviour {
+    
+    public GameObject enemy;
+    //public enum spawnPos {spawn1,spawn2};
+    //  public spawnPos[] allPos;
+    public Transform[] spawnPoint;
+
+	// Use this for initialization
+	void Start () {
+
+        for (int i = 0; i < 5; i++)
+        {
+            Instantiate(enemy, spawnPoint[i].position, Quaternion.identity);
+        }
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
